d909bf24745fc26baf5a80958022388f110bd87d
Neil Han <neil_han@intuit.com>
2019-01-04 18:23:58 -0800

    pascals trapezoid

--- src/ca/neilhan/clojure4/pascals_trapezoid.clj
+++ src/ca/neilhan/clojure4/pascals_trapezoid.clj
@@ -2,6 +2,21 @@
   (:gen-class))
 
 (defn -main [& args]
-
+  (println
+    (take 100
+          (#(letfn [
+                    (pascals [xs]
+                      (let [l (concat xs [0]),
+                            r (concat [0] xs)]
+                        (into [] (map +' l r))
+                        ))
+                    (pascals-lazy-seq [xs]
+                      (lazy-seq (cons xs (pascals-lazy-seq (pascals xs)))))]
+              (pascals-lazy-seq %1)
+              )
+            [2 4 2]
+            )
+          )
+    )
   (println args))
 

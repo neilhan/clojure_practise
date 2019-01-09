9278f179d1aba2477faa256087281ab38bf9596c
Neil Han <neil_han@intuit.com>
2019-01-08 21:04:33 -0800

    find distinct items

---
+++ src/ca/neilhan/clojure4/find_distinct_items.clj
@@ -0,0 +1,34 @@
+(ns ca.neilhan.clojure4.find_distinct_items
+  (:gen-class))
+
+
+; good solutions:
+; #(sort-by (fn [i] (.indexOf % i)) (map first (group-by identity %)))
+(defn __
+  [xs]
+  (println (sort-by (fn [x] (.indexOf xs x)) (map first (group-by identity xs))))
+  )
+
+; my first try
+(defn ___
+  [xs]
+  (second (reduce (fn [[the_set result], x] (if (contains? the_set x)
+                                              [the_set result]
+                                              (list (conj the_set x) (conj result x))))
+          [#{} []] xs))
+  )
+
+
+; Write a function which removes the duplicates from a sequence. Order of the items must be maintained.
+
+(println 
+  (= (__ [1 2 1 3 1 2 4]) [1 2 3 4]))
+
+(println 
+  (= (__ [:a :a :b :b :c :c]) [:a :b :c]))
+
+(println 
+  (= (__ '([2 4] [1 2] [1 3] [1 3])) '([2 4] [1 2] [1 3])))
+
+(println 
+  (= (__ (range 50)) (range 50)))
